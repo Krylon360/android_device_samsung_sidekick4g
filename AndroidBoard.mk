dc6533d0cae3838286d4588809968e4854d885c9
krylon360 <krylon360@krylon360-dev-vm.(none)>
2011-06-13 18:46:23 -0600

    Initial Upload

---
+++ AndroidBoard.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/sbin/postrecoveryboot.sh
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/postrecoveryboot.sh | $(ACP)
+	$(transform-prebuilt-to-target)
+
+
---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_sidekick4g.mk
